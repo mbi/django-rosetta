64df1d79c7a6177934804abde4cb8fb24efc3066
Marco Bonetti <mbonetti@gmail.com>
2012-01-16 10:36:58 +0100

    version bump

--- rosetta/__init__.py
+++ rosetta/__init__.py
@@ -1,13 +1,7 @@
-VERSION = (0, 6, 3)
+VERSION = (0, 6, 4)
 
 
 def get_version(svn=False, limit=3):
     "Returns the version as a human-format string."
-    v = '.'.join([str(i) for i in VERSION[:limit]])
-    if svn and limit >= 3:
-        from django.utils.version import get_svn_revision
-        import os
-        svn_rev = get_svn_revision(os.path.dirname(__file__))
-        if svn_rev:
-            v = '%s.%s' % (v, svn_rev)
-    return v
+    return '.'.join([str(i) for i in VERSION[:limit]])
+    
