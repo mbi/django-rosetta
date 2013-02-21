ba972b5b6ade85d21734a1a456f1dbac6099787f
Marco Bonetti <mbonetti@gmail.com>
2013-02-21 16:46:16 +0100

    Possible fix for issues #63 and #64

--- rosetta/poutil.py
+++ rosetta/poutil.py
@@ -46,7 +46,10 @@ def find_pos(lang, project_apps=True, django_apps=False, third_party_apps=False)
     project = __import__(parts[0], {}, {}, [])
     abs_project_path = os.path.normpath(os.path.abspath(os.path.dirname(project.__file__)))
     if project_apps:
-        paths.append(os.path.abspath(os.path.join(os.path.dirname(project.__file__), 'locale')))
+        if os.path.exists(os.path.abspath(os.path.join(os.path.dirname(project.__file__), 'locale'))):
+            paths.append(os.path.abspath(os.path.join(os.path.dirname(project.__file__), 'locale')))
+        if os.path.exists(os.path.abspath(os.path.join(os.path.dirname(project.__file__), '..', 'locale'))):
+            paths.append(os.path.abspath(os.path.join(os.path.dirname(project.__file__), '..', 'locale')))
 
     # django/locale
     if django_apps:
@@ -75,7 +78,6 @@ def find_pos(lang, project_apps=True, django_apps=False, third_party_apps=False)
             app = __import__(appname, {}, {}, [])
 
         apppath = os.path.normpath(os.path.abspath(os.path.join(os.path.dirname(app.__file__), 'locale')))
-        
 
         # django apps
         if 'contrib' in apppath and 'django' in apppath and not django_apps:
@@ -84,52 +86,57 @@ def find_pos(lang, project_apps=True, django_apps=False, third_party_apps=False)
         # third party external
         if not third_party_apps and abs_project_path not in apppath:
             continue
-            
+
         # local apps
         if not project_apps and abs_project_path in apppath:
             continue
-            
-        
+
         if os.path.isdir(apppath):
             paths.append(apppath)
-            
-            
-        
-            
+
     ret = set()
     langs = (lang,)
     if u'-' in lang:
-        _l,_c =  map(lambda x:x.lower(),lang.split(u'-'))
-        langs += (u'%s_%s' %(_l, _c), u'%s_%s' %(_l, _c.upper()), )
+        _l, _c = map(lambda x: x.lower(), lang.split(u'-'))
+        langs += (u'%s_%s' % (_l, _c), u'%s_%s' % (_l, _c.upper()), )
     elif u'_' in lang:
-        _l,_c = map(lambda x:x.lower(),lang.split(u'_'))
-        langs += (u'%s-%s' %(_l, _c), u'%s-%s' %(_l, _c.upper()), )
-        
+        _l, _c = map(lambda x: x.lower(), lang.split(u'_'))
+        langs += (u'%s-%s' % (_l, _c), u'%s-%s' % (_l, _c.upper()), )
+
     paths = map(os.path.normpath, paths)
+    paths = list(set(paths))
     for path in paths:
         for lang_ in langs:
             dirname = os.path.join(path, lang_, 'LC_MESSAGES')
-            for fn in ('django.po','djangojs.po',):
+            for fn in ('django.po', 'djangojs.po',):
                 filename = os.path.join(dirname, fn)
                 if os.path.isfile(filename):
                     ret.add(os.path.abspath(filename))
     return list(ret)
 
-def pagination_range(first,last,current):
+
+def pagination_range(first, last, current):
     r = []
-    
+
     r.append(first)
-    if first + 1 < last: r.append(first+1)
-    
-    if current -2 > first and current -2 < last: r.append(current-2)
-    if current -1 > first and current -1 < last: r.append(current-1)
-    if current > first and current < last: r.append(current)
-    if current + 1 < last and current+1 > first: r.append(current+1)
-    if current + 2 < last and current+2 > first: r.append(current+2)
-    
-    if last-1 > first: r.append(last-1)
+    if first + 1 < last:
+        r.append(first + 1)
+
+    if current - 2 > first and current - 2 < last:
+        r.append(current - 2)
+    if current - 1 > first and current - 1 < last:
+        r.append(current - 1)
+    if current > first and current < last:
+        r.append(current)
+    if current + 1 < last and current + 1 > first:
+        r.append(current + 1)
+    if current + 2 < last and current + 2 > first:
+        r.append(current + 2)
+
+    if last - 1 > first:
+        r.append(last - 1)
     r.append(last)
-    
+
     r = list(set(r))
     r.sort()
     prev = 10000
