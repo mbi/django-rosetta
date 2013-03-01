c38f8128b5f3edc6fb84b628696c6a14450f98fe
Marco Bonetti <mbonetti@gmail.com>
2013-03-01 14:53:37 +0100

    have the test scripts create and provision the virtual envs

--- runtests_coverage.sh
+++ runtests_coverage.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ ! -d venv_13 ]
+then
+    virtualenv --no-site-packages --distribute --python=python2 venv_13
+    . venv_13/bin/activate
+    pip install Django==1.3 coverage python-memcached six
+    deactivate
+fi
+
 . venv_13/bin/activate
 cd testproject
 coverage run --rcfile=.coveragerc manage.py test --failfast rosetta
--- runtests_multi_venv.sh
+++ runtests_multi_venv.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
+if [ ! -d venv_13 ]
+then
+    virtualenv --no-site-packages --distribute --python=python2 venv_13
+    . venv_13/bin/activate
+    pip install Django==1.3 coverage python-memcached six
+    deactivate
+fi
+if [ ! -d venv_14 ]
+then
+    virtualenv --no-site-packages --distribute --python=python2 venv_14
+    . venv_14/bin/activate
+    pip install Django==1.4 coverage python-memcached six
+    deactivate
+fi
+if [ ! -d venv_15 ]
+then
+    virtualenv --no-site-packages --distribute --python=python2 venv_15
+    . venv_15/bin/activate
+    pip install Django==1.5 coverage python-memcached six
+    deactivate
+fi
+if [ ! -d venv_15_p3 ]
+then
+    virtualenv --no-site-packages --distribute --python=python3 venv_15_p3
+    . venv_15_p3/bin/activate
+    pip install Django==1.5 coverage python3-memcached six
+    deactivate
+fi
+
+
+
 . venv_13/bin/activate
 cd testproject
 python manage.py --version
