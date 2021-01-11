92d498df63ddb85575c6cd13eb0383ef76350ac7
Álvaro Mondéjar <mondejar1994@gmail.com>
2021-01-11 10:38:03 +0100

    Remove 'providing_args' kwarg from signals instanciation

--- rosetta/signals.py
+++ rosetta/signals.py
@@ -1,9 +1,4 @@
 from django import dispatch
 
-entry_changed = dispatch.Signal(
-    providing_args=["user", "old_msgstr", "old_fuzzy", "pofile", "language_code"]
-)
-
-post_save = dispatch.Signal(
-    providing_args=["language_code", "request"]
-)
+entry_changed = dispatch.Signal()
+post_save = dispatch.Signal()
