8c5edbf6d928ab937128b783782726c06592cc9f
Marco Bonetti <marco@cruncher.ch>
2021-01-11 10:56:02 +0100

    Add providing_args as a comment

--- rosetta/signals.py
+++ rosetta/signals.py
@@ -1,4 +1,8 @@
 from django import dispatch
 
+
+# providing_args=["user", "old_msgstr", "old_fuzzy", "pofile", "language_code"]
 entry_changed = dispatch.Signal()
+
+# providing_args=["language_code", "request"]
 post_save = dispatch.Signal()
