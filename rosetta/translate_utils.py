e234befcbccb203a4dd94d142fae001309f03f4e
Marco Bonetti <marco@cruncher.ch>
2021-03-04 14:34:52 +0100

    Fix Google translation api

--- rosetta/translate_utils.py
+++ rosetta/translate_utils.py
@@ -1,9 +1,9 @@
+import json
 import uuid
 
 from django.conf import settings
 
 import requests
-import json
 
 
 class TranslationException(Exception):
@@ -12,17 +12,23 @@ class TranslationException(Exception):
 
 def translate(text, from_language, to_language):
     AZURE_CLIENT_SECRET = getattr(settings, 'AZURE_CLIENT_SECRET', None)
-    GOOGLE_APPLICATION_CREDENTIALS_PATH = getattr(settings, 'GOOGLE_APPLICATION_CREDENTIALS_PATH', None)
+    GOOGLE_APPLICATION_CREDENTIALS_PATH = getattr(
+        settings, 'GOOGLE_APPLICATION_CREDENTIALS_PATH', None
+    )
     GOOGLE_PROJECT_ID = getattr(settings, 'GOOGLE_PROJECT_ID', None)
 
     if AZURE_CLIENT_SECRET:
         return translate_by_azure(text, from_language, to_language, AZURE_CLIENT_SECRET)
     elif GOOGLE_APPLICATION_CREDENTIALS_PATH and GOOGLE_PROJECT_ID:
-        return translate_by_google(text, from_language, to_language, GOOGLE_APPLICATION_CREDENTIALS_PATH, GOOGLE_PROJECT_ID)
+        return translate_by_google(
+            text,
+            from_language,
+            to_language,
+            GOOGLE_APPLICATION_CREDENTIALS_PATH,
+            GOOGLE_PROJECT_ID,
+        )
     else:
         raise TranslationException('No translation API service is configured.')
-
-
 
 
 def translate_by_azure(text, from_language, to_language, subscription_key):
@@ -43,20 +49,20 @@ def translate_by_azure(text, from_language, to_language, subscription_key):
     headers = {
         'Ocp-Apim-Subscription-Key': subscription_key,
         'Content-type': 'application/json',
-        'X-ClientTraceId': str(uuid.uuid4())
+        'X-ClientTraceId': str(uuid.uuid4()),
     }
 
-    url_parameters = {
-        "from": from_language,
-        "to": to_language
-    }
+    url_parameters = {"from": from_language, "to": to_language}
 
-    request_data = [
-        {"text": text}
-    ]
+    request_data = [{"text": text}]
 
     api_hostname = AZURE_TRANSLATOR_HOST + AZURE_TRANSLATOR_PATH
-    r = requests.post(api_hostname, headers=headers, params=url_parameters, data=json.dumps(request_data))
+    r = requests.post(
+        api_hostname,
+        headers=headers,
+        params=url_parameters,
+        data=json.dumps(request_data),
+    )
     api_response = json.loads(r.text)
 
     try:
@@ -71,7 +77,11 @@ def translate_by_azure(text, from_language, to_language, subscription_key):
             error_code = api_error.get("code")
             error_message = api_error.get("message")
 
-            raise TranslationException("Microsoft Translation API error: Error code {}, {}".format(error_code, error_message))
+            raise TranslationException(
+                "Microsoft Translation API error: Error code {}, {}".format(
+                    error_code, error_message
+                )
+            )
         else:
             # response body will be of the form:
             # [
@@ -89,26 +99,31 @@ def translate_by_azure(text, from_language, to_language, subscription_key):
 
             return translated_text
     except requests.exceptions.RequestException as err:
-        raise TranslationException("Error connecting to Microsoft Translation Service: {0}".format(err))
+        raise TranslationException(
+            "Error connecting to Microsoft Translation Service: {0}".format(err)
+        )
 
 
-def translate_by_google(text, input_language, output_language, creadentials_path, project_id):
-    from google.cloud import translate_v3beta1 as google_translate
+def translate_by_google(
+    text, input_language, output_language, creadentials_path, project_id
+):
+    from google.cloud import translate as google_translate
 
-    # using new v3beta Translation API
-    client = google_translate.TranslationServiceClient.from_service_account_json(creadentials_path)
-    parent = client.location_path(project_id, 'global')
-
+    client = google_translate.TranslationServiceClient.from_service_account_json(
+        creadentials_path
+    )
+    parent = "projects/{}/locations/{}".format(project_id, 'global')
     try:
         api_response = client.translate_text(
-            parent=parent,
-            contents=[text],
-            mime_type='text/plain',
-            source_language_code=input_language,
-            target_language_code=output_language
+            request=dict(
+                parent=parent,
+                contents=[text],
+                mime_type='text/plain',
+                source_language_code=input_language,
+                target_language_code=output_language.split('.', 1)[0],
+            )
         )
     except Exception as e:
         raise TranslationException('Google API error: {}'.format(e))
     else:
         return str(api_response.translations[0].translated_text)
-
