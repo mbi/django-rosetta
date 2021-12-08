119292a762bde33e42d8483d0b01f1c4d2f99d7d
Marco Bonetti <marco@cruncher.ch>
2021-12-08 09:24:34 +0100

    update test matrix

--- rosetta/__init__.py
+++ rosetta/__init__.py
@@ -11,4 +11,4 @@ VERSION = (0, 9, 8)
 
 def get_version(limit=3):
     """Return the version as a human-format string."""
-    return '.'.join([str(i) for i in VERSION[:limit]])
+    return ".".join([str(i) for i in VERSION[:limit]])
