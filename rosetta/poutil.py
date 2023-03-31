b70d283ee8c4ed5b7e9762cf1c9845d2af3d2141
Jirka Schaefer <jirka@tschitschereengreen.com>
2023-03-31 14:53:36 +0200

    allow setting to override case sensitive detection

--- rosetta/poutil.py
+++ rosetta/poutil.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 from datetime import datetime
 
 import django
@@ -70,9 +71,18 @@ def find_pos(lang, project_apps=True, django_apps=False, third_party_apps=False)
                 )
             )
 
+    # is OS case sensitive? settings preferred over auto detection
     case_sensitive_file_system = getattr(
-        settings, "ROSETTA_CASE_SENSITIVE_FILESYSTEM", False
+        settings, "ROSETTA_CASE_SENSITIVE_FILESYSTEM", None
     )
+
+    # in case of no settings, attempt auto detection
+    if case_sensitive_file_system is None:
+        case_sensitive_file_system = True
+        tmphandle, tmppath = tempfile.mkstemp()
+        if os.path.exists(tmppath.upper()):
+            # Case insensitive file system.
+            case_sensitive_file_system = False
 
     # django/locale
     if django_apps:
